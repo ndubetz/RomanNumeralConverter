1c43e07861c70ef39de92178df15f19e26776ec3
NATE DUBETZ <nbdubetz@gmail.com>
2015-11-09 22:50:52 -0500

    Convert Arabic numbers less than 10
    ArabictoRomanNumberConverter now converts Arabic numbers 1-9.

--- ArabicToRomanConverter/src/main/ArabicToRomanNumberConverter.java
+++ ArabicToRomanConverter/src/main/ArabicToRomanNumberConverter.java
@@ -5,13 +5,23 @@ public class ArabicToRomanNumberConverter {
 	public String convertToRomanNumeral(int numberToConvert) {
 		String convertedString = "";
 		while(numberToConvert > 0){
-			if(numberToConvert == 4){
-				convertedString += "IV";
-				numberToConvert -= 4;
+			if (numberToConvert == 9){
+				convertedString += "IX";
+				numberToConvert -= 9;
+			}
+			else if(numberToConvert > 4){
+				convertedString += "V";
+				numberToConvert -= 5;
 			}
 			else{
-				convertedString += "I";
-				numberToConvert--;
+				if(numberToConvert == 4){
+					convertedString += "IV";
+					numberToConvert -= 4;
+				}
+				else{
+					convertedString += "I";
+					numberToConvert--;
+				}
 			}
 		}
 		return convertedString;
--- ArabicToRomanConverterTest/src/main/ArabicToRomanNumberConverterTest.java
+++ ArabicToRomanConverterTest/src/main/ArabicToRomanNumberConverterTest.java
@@ -15,4 +15,20 @@ public class ArabicToRomanNumberConverterTest extends TestCase{
 		String expectedRomanNumeral2 = "IV";
 		assertEquals(expectedRomanNumeral2, arabicToRomanNumberConverter.convertToRomanNumeral(numberToConvert2));
 	}
+	
+	public void testConvert_CanConvertNumbersThatAreLessThan10() throws Exception {
+		ArabicToRomanNumberConverter arabicToRomanNumberConverter = new ArabicToRomanNumberConverter();
+		
+		int numberToConvert1 = 5;
+		String expectedRomanNumeral1 = "V";
+		assertEquals(expectedRomanNumeral1, arabicToRomanNumberConverter.convertToRomanNumeral(numberToConvert1));
+		
+		int numberToConvert2 = 7;
+		String expectedRomanNumeral2 = "VII";
+		assertEquals(expectedRomanNumeral2, arabicToRomanNumberConverter.convertToRomanNumeral(numberToConvert2));
+		
+		int numberToConvert3 = 9;
+		String expectedRomanNumeral3 = "IX";
+		assertEquals(expectedRomanNumeral3, arabicToRomanNumberConverter.convertToRomanNumeral(numberToConvert3));
+	}
 }
