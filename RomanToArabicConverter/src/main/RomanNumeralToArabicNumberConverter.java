34fbe789083496735c747263f9b5bf4e76d41a63
NATE DUBETZ <nbdubetz@gmail.com>
2015-11-10 23:50:44 -0500

    Refactor RomanNumeralToArabicNumberConverter
    Consolidated tests for RomanNumeralToArabicNumberConverter and
    refactored duplicate code out of RomanNumeralToArabicNumberConverter

--- RomanToArabicConverter/src/main/RomanNumeralToArabicNumberConverter.java
+++ RomanToArabicConverter/src/main/RomanNumeralToArabicNumberConverter.java
@@ -1,49 +1,60 @@
 package main;
 
+import java.util.Stack;
+
 public class RomanNumeralToArabicNumberConverter {
+
+	private Stack<Character> romanNumeralAsStackOfCharacters;
 
 	public int convertToArabicNumber(String romanNumeralToConvert) {
 		int convertedNumber = 0;
 		
-		char[] romanNumeralChars = romanNumeralToConvert.toCharArray();
-		for(int i = 0; i < romanNumeralChars.length; i++){
-			if(romanNumeralChars[i] == 'I'){
-				if(i < romanNumeralChars.length - 1){
-					if(romanNumeralChars[i + 1] == 'V'){
-						convertedNumber += 4;
-						i++;
-					}
-					else if (romanNumeralChars[i + 1] == 'X'){
-						convertedNumber += 9;
-						i++;
-					}
-					else{
-						convertedNumber += 1;
-					}
-				}else{
-					convertedNumber += 1;
-				}
-			}
-			else if (romanNumeralChars[i] == 'V'){
-				convertedNumber += 5;
-			}
-			else if (romanNumeralChars[i] == 'X'){
-				if(romanNumeralChars[i + 1] == 'L'){
-					convertedNumber += 40;
-					i++;
-				}
-				else if (romanNumeralChars[i + 1] == 'C'){
-					convertedNumber += 90;
-					i++;
-				}
-				else{
-					convertedNumber += 10;
-				}
-			}else{
-				convertedNumber += 50;
-			}
+		romanNumeralAsStackOfCharacters = convertToStackOfCharacters(romanNumeralToConvert);
+		
+		while(!romanNumeralAsStackOfCharacters.isEmpty()){
+			convertedNumber += parseCharIntoIntAndSubtractIfNecessary(romanNumeralAsStackOfCharacters);
 		}
+
 		return convertedNumber;
 	}
+	
+	private Stack<Character> convertToStackOfCharacters(String romanNumeralToConvert){
+		char[] romanNumeralChars = romanNumeralToConvert.toCharArray();
+		Stack<Character> stack = new Stack<Character>();
+		for (Character character : romanNumeralChars) {
+			stack.push(character);
+		}
+		
+		return stack;
+	}
 
+	private int parseCharIntoIntAndSubtractIfNecessary(Stack<Character> stackOfChars) {
+		Character romanNumeral = romanNumeralAsStackOfCharacters.pop();
+		if (romanNumeral == 'C'){
+			return actuallyParseNumeralToIntegerAndSubtractIfNecessary('X', 100, 10);
+		} 
+		else if (romanNumeral == 'L'){
+			return actuallyParseNumeralToIntegerAndSubtractIfNecessary('X', 50, 10);
+		}
+		else if (romanNumeral == 'X'){
+			return actuallyParseNumeralToIntegerAndSubtractIfNecessary('I', 10, 1);
+		} 
+		else if (romanNumeral == 'V'){
+			return actuallyParseNumeralToIntegerAndSubtractIfNecessary('I', 5, 1);
+		}
+		else{
+			return 1;
+		}
+	}
+	
+	private int actuallyParseNumeralToIntegerAndSubtractIfNecessary(char subtractingChar, 
+			int valueOfNumeral, int valueToSubtract){
+		if (!romanNumeralAsStackOfCharacters.isEmpty() && romanNumeralAsStackOfCharacters.peek() == subtractingChar){
+			romanNumeralAsStackOfCharacters.pop();
+			return valueOfNumeral - valueToSubtract;
+		}
+		else{
+			return valueOfNumeral;
+		}
+	}
 }
--- RomanToArabicConverterTest/src/main/RomanNumeralToArabicNumberConverterTest.java
+++ RomanToArabicConverterTest/src/main/RomanNumeralToArabicNumberConverterTest.java
@@ -4,40 +4,16 @@ import junit.framework.TestCase;
 
 public class RomanNumeralToArabicNumberConverterTest extends TestCase {
 	
-	public void testConvert_CanConvertNumeralsLessThanX() throws Exception {
+	public void testConvert_CanConvertNumbersLessThan100() throws Exception {
 		RomanNumeralToArabicNumberConverter romanNumeralToArabicNumberConverter 
 			= new RomanNumeralToArabicNumberConverter();
-		String [] romanNumeralsToConvert = {"II", "IV", "VI", "VIII", "IX"};
-		int[] expectedArabicNumbers = {2,4,6,8,9};
+		String [] romanNumeralsToConvert = {"II", "IV", "VI", "VIII", "IX", "XIV", "XXXIII", "XLI", 
+				"XLIV", "XLIX", "LIV", "LXXX", "LXXXVII", "XCIV", "XCIX"};
+		int[] expectedArabicNumbers = {2,4,6,8,9,14,33,41,44,49,54,80,87,94,99};
 
 		for(int i = 0; i < expectedArabicNumbers.length; i++){
 			assertEquals(expectedArabicNumbers[i], 
 				romanNumeralToArabicNumberConverter.convertToArabicNumber(romanNumeralsToConvert[i]));
-		}
-	}
-	
-	public void testConvert_CanConvertNumeralsLessThanL() throws Exception {
-		RomanNumeralToArabicNumberConverter romanNumeralToArabicNumberConverter 
-		= new RomanNumeralToArabicNumberConverter();
-		String [] romanNumeralsToConvert = {"XIV", "XXXIII", "XLI", 
-				"XLIV", "XLIX"};
-		int[] expectedArabicNumbers = {14,33,41,44,49};
-		
-		for(int i = 0; i < expectedArabicNumbers.length; i++){
-			assertEquals(expectedArabicNumbers[i], 
-					romanNumeralToArabicNumberConverter.convertToArabicNumber(romanNumeralsToConvert[i]));
-		}
-	}
-	
-	public void testConvert_CanConvertNumeralsLessThanC() throws Exception {
-		RomanNumeralToArabicNumberConverter romanNumeralToArabicNumberConverter 
-		= new RomanNumeralToArabicNumberConverter();
-		String [] romanNumeralsToConvert = {"LIV", "LXXIX", "LXXXVII", "XCIV", "XCIX"};
-		int[] expectedArabicNumbers = {54,79,87,94,99};
-		
-		for(int i = 0; i < expectedArabicNumbers.length; i++){
-			assertEquals(expectedArabicNumbers[i], 
-					romanNumeralToArabicNumberConverter.convertToArabicNumber(romanNumeralsToConvert[i]));
 		}
 	}
 }
