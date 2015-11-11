f8268c41d14d8cb03f98f46ba14df8b0d703e73d
NATE DUBETZ <nbdubetz@gmail.com>
2015-11-11 01:25:29 -0500

    RomanNumeralValidator full validation
    The RomanNumeralValidator validates using a master regex. All cases
    covered.

--- RomanToArabicConverter/src/main/RomanNumeralValidator.java
+++ RomanToArabicConverter/src/main/RomanNumeralValidator.java
@@ -6,8 +6,8 @@ import java.util.regex.Pattern;
 public class RomanNumeralValidator {
 
 	public boolean isAValidRomanNumeral(String romanNumeralToValidate) {
-		String validCharacters = "[MDCLXVI]+";
-		return Pattern.matches(validCharacters, romanNumeralToValidate.toUpperCase());
+		String romanNumeralRegex = "M{0,3}(D{0,1}C{0,3}|CD{1}|CM{1})(L{0,1}X{0,3}|XL{1}|XC{1})(V{0,1}I{0,3}|IV{1}|IX{1})";
+		return Pattern.matches(romanNumeralRegex, romanNumeralToValidate.toUpperCase()) && !romanNumeralToValidate.isEmpty();
 	}
 
 }
--- RomanToArabicConverterTest/src/main/RomanNumeralValidatorTest.java
+++ RomanToArabicConverterTest/src/main/RomanNumeralValidatorTest.java
@@ -4,33 +4,71 @@ import junit.framework.TestCase;
 
 public class RomanNumeralValidatorTest extends TestCase{
 	
+	private RomanNumeralValidator romanNumeralValidator;
+	
+	@Override
+	protected void setUp() throws Exception {
+		romanNumeralValidator = new RomanNumeralValidator();
+		super.setUp();
+	}
+
 	public void testValidate_ReturnsTrueForValidRomanNumerals_RegressionTest() throws Exception {
-		RomanNumeralValidator romanNumeralValidator = new RomanNumeralValidator();
+		String[] romanNumeralsToValidate = {"MMXV", "MCMXLIX", "MMMCCCXXXIII", "DLV", "MDCLXVI", "I", "MMMCMXCIX"};
 		
-		String[] romanNumeralsToValidate = {"MMXV", "MCMXLIX", "MMMCCCXXXIII", "DLV", "MDCLXVI"};
-		
-		for(int i = 0 ; i < romanNumeralsToValidate.length; i++){
-			assertTrue(romanNumeralValidator.isAValidRomanNumeral(romanNumeralsToValidate[i]));
-		}
+		assertTrue(romanNumeralValidator.isAValidRomanNumeral(romanNumeralsToValidate[0]));
+		assertTrue(romanNumeralValidator.isAValidRomanNumeral(romanNumeralsToValidate[1]));
+		assertTrue(romanNumeralValidator.isAValidRomanNumeral(romanNumeralsToValidate[2]));
+		assertTrue(romanNumeralValidator.isAValidRomanNumeral(romanNumeralsToValidate[3]));
+		assertTrue(romanNumeralValidator.isAValidRomanNumeral(romanNumeralsToValidate[4]));
+		assertTrue(romanNumeralValidator.isAValidRomanNumeral(romanNumeralsToValidate[5]));
+		assertTrue(romanNumeralValidator.isAValidRomanNumeral(romanNumeralsToValidate[6]));
 	}
 	
-	public void testValidate_ReturnsFalseIfIllegalCharacterInString() throws Exception {
-		RomanNumeralValidator romanNumeralValidator = new RomanNumeralValidator();
+	public void testValidate_ReturnsFalseIfIllegalCharacterInString_OrEmptyString() throws Exception {
+		String[] illegalRomanNumeralsToValidate = {"XXB", "X1M", "MMM0", "ABC", ""};
 		
-		String[] illegalRomanNumeralsToValidate = {"XXB", "X1M", "MMM0", "ABC", "@#$"};
-		
-		for(int i = 0 ; i < illegalRomanNumeralsToValidate.length; i++){
-			assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[i]));
-		}
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[0]));
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[1]));
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[2]));
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[3]));
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[4]));
 	}
 	
 	public void testValidate_ConvertsToUppercaseBeforeAttemptingToValidate() throws Exception {
-		RomanNumeralValidator romanNumeralValidator = new RomanNumeralValidator();
-		
 		String[] romanNumeralsToValidate = {"xiv", "cmLxXxiv", "Cd", "dCi"};
 		
-		for(int i = 0 ; i < romanNumeralsToValidate.length; i++){
-			assertTrue(romanNumeralValidator.isAValidRomanNumeral(romanNumeralsToValidate[i]));
-		}
+		assertTrue(romanNumeralValidator.isAValidRomanNumeral(romanNumeralsToValidate[0]));
+		assertTrue(romanNumeralValidator.isAValidRomanNumeral(romanNumeralsToValidate[1]));
+		assertTrue(romanNumeralValidator.isAValidRomanNumeral(romanNumeralsToValidate[2]));
+		assertTrue(romanNumeralValidator.isAValidRomanNumeral(romanNumeralsToValidate[3]));
+	}
+	
+	public void testValidate_ReturnsFalseIfThereTooManyOnesSymbols() throws Exception {
+		String[] illegalRomanNumeralsToValidate = {"MMMM", "XXXX", "CCCC", "IIII"};
+		
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[0]));
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[1]));
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[2]));
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[3]));
+	}
+	
+	public void testValidate_ReturnsFalseIfThereTooManyFiveSymbols() throws Exception {
+		String[] illegalRomanNumeralsToValidate = {"MMVV", "LL", "DD", "DLVV"};
+		
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[0]));
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[1]));
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[2]));
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[3]));
+	}
+	
+	public void testValidate_ReturnsFalseIfThereIllegalSubtractions() throws Exception {
+		String[] illegalRomanNumeralsToValidate = {"MMIIV", "LIL", "CID", "MIM", "XXL", "DM"};
+		
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[0]));
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[1]));
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[2]));
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[3]));
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[4]));
+		assertFalse(romanNumeralValidator.isAValidRomanNumeral(illegalRomanNumeralsToValidate[5]));
 	}
 }
